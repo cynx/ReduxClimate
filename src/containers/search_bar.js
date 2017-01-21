2186e24698ee8491657d0970ab53c7272b21ca47
echosteg <email@echosteg.com>
2017-01-21 22:06:51 +0530

    integrate API

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,8 +3,10 @@
  */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props){
         super(props);
         this.state = { term: '' };
@@ -17,6 +19,8 @@ export default class SearchBar extends Component {
 
     onFormSubmit(event){
         event.preventDefault();
+        this.props.fetchWeather(this.state.term);
+        this.setState({term:''});
     }
 
     render(){
@@ -32,3 +36,8 @@ export default class SearchBar extends Component {
     }
 }
 
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
